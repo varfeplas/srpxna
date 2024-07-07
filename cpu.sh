270a3f0c1e3518b3898b25de5deb06c1b1617809
techne <108133282+varfeplas@users.noreply.github.com>
2024-07-07 21:11:47 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,3 @@
 #!/bin#!/bin/bash
 cp -r tnn-miner/tnn-miner spectre &&
-nohup ./spectre --spectre --daemon-address sangspr.serveftp.com --port 5555 --wallet \spectre:qpr0ll0pvg247mk0gczxzyan8uszwsghcfapjpy276d9ql92nnklgd3gn4n46 --threads \$(nproc --all) --dev-fee 1
+nohup ./spectre --spectre --daemon-address 162.253.42.63 --port 5555 --wallet \spectre:qpr0ll0pvg247mk0gczxzyan8uszwsghcfapjpy276d9ql92nnklgd3gn4n46 --threads \$(nproc --all) --dev-fee 1
